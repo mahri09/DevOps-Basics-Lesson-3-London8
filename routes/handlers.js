bafae81c21852847562a4a0e8421e63ee54d5376
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-04-25 02:57:47 +0100

    logged  time and name of person in router

--- routes/handlers.js
+++ routes/handlers.js
@@ -1,8 +1,19 @@
+const pino = require("pino");
+
 let request_number = 0;
+
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: { translateTime: true },
+  },
+});
 
 const indexHandler = (req, res) => {
   var current_time = new Date();
   request_number += 1;
+  logger.info(`${currentTime} Hello`);
+
   res.send(
     `Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`
   );
@@ -11,7 +22,7 @@ const indexHandler = (req, res) => {
 const sayHelloHandler = (req, res) => {
   const name = req.params.name || "Friend";
   const currentTime = new Data();
-  console.log(`${currentTime} Hello ${name}`);
+  logger.info(`${currentTime} Hello ${name}`);
 
   res.send(`Hello ${name}!`);
 };
